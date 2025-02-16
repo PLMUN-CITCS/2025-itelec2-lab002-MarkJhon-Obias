3224365cc3a49a13d655c770eb0783c925b8a07a
obiasmarkjhon_bsit@plmun.edu.ph <obiasmarkjhon_bsit@plmun.edu.ph>
2025-02-16 22:03:01 +0800

    Submitting Python Session 03 - Exercise 01

---
+++ problem_02.py
@@ -0,0 +1,12 @@
+# MARKJHON OBIAS
+# ITELEC2
+# Problem Set 01 - Problem 01
+# Simple Calculator Program
+
+def main():
+    print("Square the Number Program")
+    number = int(input("Enter a number: "))
+    print(f"The square of {number} is {(number * number):.2f}")
+
+if __name__ == "__main__":
+    main()
